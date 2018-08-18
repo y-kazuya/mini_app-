47873cda66d2bae8680dfeb7bb6d857d9be8859d
y-kazuya <y.kazuya.0124@au.com>
2018-08-18 17:50:51 +0900

    Create post controller &models
    Postコントローラとモデルの作成
    データベースの作成
    アソシエーションの設定

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module MiniApp
   class Application < Rails::Application
+    config.generators do |g|
+      g.stylesheets false
+      g.helper false
+      g.test_framework false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
